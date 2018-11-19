704201e7a85ed1964c1b911061fac288c75a6b3d
hendercine <james@hendercine.com>
2018-11-19 15:22:23 -0800

    feat($main): begin implementing MainActivity UI

--- app/src/main/java/com/hendercine/android/codeword/MainActivity.java
+++ app/src/main/java/com/hendercine/android/codeword/MainActivity.java
@@ -1,13 +1,34 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 11/18/18 10:29 PM
+ */
+
 package com.hendercine.android.codeword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Adapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.alphabet_rv_grid)
+    RecyclerView mAlphabetGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 8);
+        AlphabetRVAdapter adapter = new AlphabetRvAdapter(mAlphabetArray);
     }
 }
