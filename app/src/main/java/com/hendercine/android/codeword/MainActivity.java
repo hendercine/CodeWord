eeb5c0a0dad772465b10f995b97fc6bec8f557d1
hendercine <james@hendercine.com>
2018-11-19 17:15:19 -0800

    feat($main): implement guess check methods

--- app/src/main/java/com/hendercine/android/codeword/MainActivity.java
+++ app/src/main/java/com/hendercine/android/codeword/MainActivity.java
@@ -10,10 +10,43 @@ package com.hendercine.android.codeword;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.guess_edit_text)
+    EditText mGuessInput;
+    @BindView(R.id.guess_button)
+    Button mGuessButton;
+    @BindView(R.id.letter_1)
+    TextView mLetterOne;
+    @BindView(R.id.letter_2)
+    TextView mLetterTwo;
+    @BindView(R.id.letter_3)
+    TextView mLetterThree;
+    @BindView(R.id.letter_4)
+    TextView mLetterFour;
+    @BindView(R.id.letter_5)
+    TextView mLetterFive;
+    @BindView(R.id.letter_6)
+    TextView mLetterSix;
+    @BindView(R.id.countdown_view)
+    TextView mCountdown;
+
+    private Editable mGuessStr;
+    private String mCodeWord = "Linked";
+    private int mCount;
+    private List<Integer> mCorrectGuessPositions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +54,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        if (savedInstanceState == null) {
+            mCount = 6;
+        }
+        mCountdown.setText(String.valueOf(mCount));
+        mGuessInput.setActivated(true);
+        mGuessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGuessStr = mGuessInput.getText();
+                ArrayList<Integer> positions = (ArrayList<Integer>) checkGuess(mGuessStr
+                        .charAt(0));
+                if (positions != null) {
+                    revealCorrectPositions(positions);
+                }
+            }
+        });
+    }
+
+    private List<Integer> checkGuess(char guessLetter) {
+
+        mCorrectGuessPositions = new ArrayList<>();
+        for (int i = 0; i < mCodeWord.length(); i++) {
+            if (mCodeWord.charAt(i) == guessLetter) {
+                mCorrectGuessPositions.add(i);
+            }
+        }
+
+        return mCorrectGuessPositions;
+    }
+
+    private void revealCorrectPositions(ArrayList<Integer>
+                                                correctGuessPositions) {
+
+        for (int i = 0; i < correctGuessPositions.size(); i++) {
+            if (correctGuessPositions.get(i) == 0) {
+                mLetterOne.setText(mCodeWord.charAt(0));
+                mLetterOne.setVisibility(View.VISIBLE);
+            } else if (correctGuessPositions.get(i) == 1) {
+                mLetterTwo.setText(mCodeWord.charAt(1));
+                mLetterTwo.setVisibility(View.VISIBLE);
+            } else if (correctGuessPositions.get(i) == 2) {
+                mLetterThree.setText(mCodeWord.charAt(2));
+                mLetterThree.setVisibility(View.VISIBLE);
+            } else if (correctGuessPositions.get(i) == 3) {
+                mLetterFour.setText(mCodeWord.charAt(3));
+                mLetterFour.setVisibility(View.VISIBLE);
+            } else if (correctGuessPositions.get(i) == 4) {
+                mLetterFive.setText(mCodeWord.charAt(4));
+                mLetterFive.setVisibility(View.VISIBLE);
+            } else if (correctGuessPositions.get(i) == 5) {
+                mLetterSix.setText(mCodeWord.charAt(5));
+                mLetterSix.setVisibility(View.VISIBLE);
+            } else {
+                mCountdown.setText(String.valueOf(mCount - 1));
+            }
+        }
     }
 }
